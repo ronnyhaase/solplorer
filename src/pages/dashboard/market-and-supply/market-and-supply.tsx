2444ec3636b8175dd9cded885ae76c25955dc6ca
Ronny Haase <ronnyhaase@fastmail.com>
2022-03-26 18:57:44 +0100

    Improve responsiveness of markets widget

--- src/pages/dashboard/market-and-supply/market-and-supply.tsx
+++ src/pages/dashboard/market-and-supply/market-and-supply.tsx
@@ -10,16 +10,18 @@ const MarketAndSupply = () => {
 
   return (
     <Panel>
-      <div className="d-flex">
-        <div>
-          <div className="mb-md pb-md border-solid border-0 border-b border-inset">
-            <Market marketData={marketData} />
-          </div>
-          <div>
-            <Supply />
+      <div className="d-flex flex-wrap">
+        <div className="w-1/1 lg:w-1/3 pr-md">
+          <div className="d-flex flex-wrap mb-md lg:mb-0">
+            <div className="w-1/2 lg:w-1/1 lg:mb-md lg:pb-md border-solid border-0 lg:border-b border-inset">
+              <Market marketData={marketData} />
+            </div>
+            <div className="w-1/2 lg:w-1/1">
+              <Supply />
+            </div>
           </div>
         </div>
-        <div className="grow ml-md bg-inset">
+        <div className="w-1/1 lg:w-2/3 bg-inset" style={{ minHeight: '250px' }}>
           {marketData ? (<PriceChart history={marketData.history} />) : null}
         </div>
       </div>
