fceadcd78d0c9b1c351baf15028006a5afb16096
Ronny Haase <ronnyhaase@fastmail.com>
2022-04-23 22:19:55 +0200

    [solplorer] Fix illegal character and use next/image for img

--- solplorer/src/components/layout/Layout.tsx
+++ solplorer/src/components/layout/Layout.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head"
+import Image from "next/image"
 import Link from "next/link"
 import Container from "../container"
+
+import logoImg from '../../../public/logo-32.png'
 
 const Layout = ({ children }) => {
   return (
@@ -11,7 +14,7 @@ const Layout = ({ children }) => {
       <header className="m-0 py-lg">
         <Link href="/">
           <a className="d-flex items-center justify-center no-underline text-color-inherit">
-            <img alt="Solplorer Logo" src="/logo-32.png" />
+            <Image alt="Solplorer Logo" src={logoImg} />
             <h1 className="ml-sm">
               Solplorer
             </h1>
@@ -28,7 +31,7 @@ const Layout = ({ children }) => {
           </p>
           <p>
             Solplorer is free software under <a href="https://www.gnu.org/licenses/gpl-3.0">GNU
-            General Public License</a> version 3 and you're invited
+            General Public License</a> version 3 and you´re invited
             <a href="https://github.com/ronnyhaase/solplorer"> to contribute</a>.<br />
             This program comes with ABSOLUTELY NO WARRANTY.
           </p>
