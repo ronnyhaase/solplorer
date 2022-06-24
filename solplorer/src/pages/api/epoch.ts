e9255f863963b7fe78362aadd4d5ddaf139c0466
Ronny Haase <ronnyhaase@fastmail.com>
2022-06-24 16:23:54 +0200

    [solplorer] Fix type issues

--- solplorer/src/pages/api/epoch.ts
+++ solplorer/src/pages/api/epoch.ts
@@ -2,7 +2,7 @@ import { default as request } from 'got'
 
 const handler = async (_, res) => {
   res.status(200).json(
-   (await request(`${process.env.API_URL}/solana/epoch`).json()).data
+   (await request(`${process.env.API_URL}/solana/epoch`).json() as any).data
   )
 }
 
