61f3d246afdbfc8b5610cbe637ae46b50c2ab0d7
Ronny Haase <ronnyhaase@fastmail.com>
2022-08-11 22:55:14 +0200

    [solplorer] Add TPS and epoch slot range
    - Add TPS and epoch slot range
    - Make values fade in on change

--- solplorer/src/pages/dashboard/epoch.tsx
+++ solplorer/src/pages/dashboard/epoch.tsx
@@ -12,13 +12,19 @@ const Epoch = ({ statsData }) => {
           <div>
             <span className="text-muted">Slot Height</span>
           </div>
-          <NumberDisplay as="div" className="font-mono text-xl" val={slotHeight} />
+          <NumberDisplay as="div" className="FadeIn font-mono text-xl" val={slotHeight} key={Math.random()} />
         </div>
         <div className="w-1/1 lg:w-auto sm:w-1/2 lg:mr-md pt-md sm:pt-0 lg:pr-md border-0 border-t sm:border-t-0 lg:border-r border-solid border-inset">
           <div>
             <span className="text-muted">Transactions</span>
           </div>
-          <NumberDisplay as="div" className="font-mono text-xl" val={transactionsCount} />
+          <div>
+            <span className="d-inline-block sm:text-center fadeIn">
+              <NumberDisplay className="FadeIn font-mono text-xl" val={transactionsCount} key={Math.random()} /><br />
+              <span className="text-muted">TPS: </span>
+              <NumberDisplay className="FadeIn" val={tps}  key={Math.random()} />
+            </span>
+          </div>
         </div>
         <div className="mt-md lg:mt-0 pt-md lg:pt-0 border-0 border-solid border-inset grow border-t lg:border-t-0">
           <div>
@@ -34,6 +40,10 @@ const Epoch = ({ statsData }) => {
               <Progress className="grow" min={0} max={100} value={epochProgress ? epochProgress : 0}></Progress>
             </div>
             <Display className="text-inset text-xl">{nextEpoch}</Display>
+          </div>
+          <div className="text-center">
+            <span className="text-muted">Slot Range: </span>
+            <NumberDisplay val={slotRangeStart} /> - <NumberDisplay val={slotRangeEnd} />
           </div>
         </div>
       </div>
