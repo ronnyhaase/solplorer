3a27dc267528d0edf2b50c6cfbc39213bda97524
Ronny Haase <ronnyhaase@fastmail.com>
2022-09-05 23:00:09 +0200

    [solade] SLPL-12 - Add further epoch data
    - main program
    - vote and non-vote transaction count
    - program aggregation (call per program)

--- solade/src/solana/blocks/index.ts
+++ solade/src/solana/blocks/index.ts
@@ -1,22 +1,68 @@
-import { epochByBlock, lamportToSol } from '../helpers';
+import {
+  epochByBlock,
+  getProgramFromLog,
+  getTransactionMainProgram,
+  isVoteTransaction,
+  lamportToSol
+} from '../helpers';
+
+
 
 export const buildBlockTransaction = (tx) => ({
   signature: tx.transaction.signatures[0],
   sender: tx.transaction.message.accountKeys[0],
+  mainProgram: getTransactionMainProgram(tx),
   error: !!tx.meta?.err || false,
   fee: lamportToSol(tx.meta?.fee),
 });
 
-export function buildBlock (blockNo, rawBlock) {
-  let transactionsCountFailed = 0, transactionsCountSuccess = 0
-  rawBlock.transactions.map((tx) => {
+export const getBlockTransactionsAggregation = (rawTransactions) => {
+  let transactionsCountFailed = 0, transactionsCountSuccess = 0;
+  let transactionsCountVote = 0, transactionsCountNonVote = 0;
+  let programAggregation: Object = {};
+
+  rawTransactions.map((tx) => {
     if (tx.meta?.err) transactionsCountFailed += 1
-    else transactionsCountSuccess += 1
-  })
+    else transactionsCountSuccess += 1;
+
+    if (isVoteTransaction(tx)) transactionsCountVote += 1
+    else transactionsCountNonVote += 1;
+
+    const program = getTransactionMainProgram(tx);
+    if (Array.isArray(tx.meta?.logMessages)) {
+      tx.meta.logMessages.map(msg => {
+        const program = getProgramFromLog(msg);
+        if (program) {
+          if (!program) {}
+          else if (programAggregation.hasOwnProperty(program)) programAggregation[program] += 1
+          else programAggregation[program] = 1;
+        }
+      })
+    }
+
+  });
+
+  return [
+    programAggregation,
+    transactionsCountFailed,
+    transactionsCountSuccess,
+    transactionsCountVote,
+    transactionsCountNonVote,
+  ];
+}
+
+export function buildBlock (blockNo, rawBlock) {
+  const [
+    programAggregation = {},
+    transactionsCountFailed = 0,
+    transactionsCountSuccess = 0,
+    transactionsCountVote = 0,
+    transactionsCountNonVote = 0,
+  ] = getBlockTransactionsAggregation(rawBlock.transactions);
 
   return {
     _raw: rawBlock,
-    base: {
+    baseData: {
       blockNo,
       epoch: epochByBlock(blockNo),
       ts: rawBlock.blockTime * 1000,
@@ -26,9 +72,12 @@ export function buildBlock (blockNo, rawBlock) {
       leaderReward: lamportToSol(rawBlock.rewards[0]?.lamports),
       transactionsCount: rawBlock.transactions.length,
     },
+    programAggregation,
     transactions: rawBlock.transactions.map(buildBlockTransaction),
     transactionsCount: rawBlock.transactions.length,
     transactionsCountFailed,
     transactionsCountSuccess,
+    transactionsCountVote,
+    transactionsCountNonVote
   }
 };
--- solade/src/solana/constants.ts
+++ solade/src/solana/constants.ts
@@ -1,2 +1,3 @@
 export const BLOCKS_PER_EPOCH = 432000;
 export const SOL_PER_LAMPORT = 0.000000001;
+export const RE_PROGRAM_INVOCATION = /^Program\ ([a-zA-Z0-9]+)\ invoke \[\d+\]/;
