481268d9f47f736b7cc5998a897621ee048309ad
Ronny Haase <ronnyhaase@fastmail.com>
2022-11-04 11:45:03 +0100

    SLPL-31 - [etl] Initialize Python env and implement scheduler script

---
+++ etl/main.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os
+import subprocess
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+def build_node_job(job_name):
+    return lambda: subprocess.run(["node", os.getcwd() + "/jobs/src" + job_name])
+
+
+def main():
+    """
+    main
+    """
+    scheduler = BlockingScheduler(
+        {
+            "apscheduler.timezone": "UTC",
+            "apscheduler.job_defaults.max_instances": "1",
+        }
+    )
+
+    scheduler.add_job(build_node_job("stats"), "interval", seconds=15)
+    scheduler.add_job(build_node_job("epoch"), "interval", minutes=10)
+    scheduler.add_job(build_node_job("markets"), "interval", minutes=15)
+    scheduler.add_job(build_node_job("news"), "interval", hours=1)
+    scheduler.add_job(build_node_job("supply"), "interval", days=1)
+    scheduler.add_job(build_node_job("tokens"), "interval", days=1, minutes=5)
+    scheduler.add_job(build_node_job("tvl"), "interval", days=1, minutes=10)
+    scheduler.add_job(build_node_job("validators"), "interval", days=1, minutes=30)
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+
+if __name__ == "__main__":
+    main()
