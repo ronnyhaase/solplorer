4207cc8b0c1569bb35967c23df694ebbdae12fdf
Ronny Haase <ronnyhaase@fastmail.com>
2022-12-09 09:21:57 +0100

    [etl] Merge TVL and price histories into one in markets job

--- etl/etl/jobs/markets.py
+++ etl/etl/jobs/markets.py
@@ -2,27 +2,30 @@ import json
 
 import httpx
 from dotenv import load_dotenv
+from toolz import merge
 
 from etl.connector import get_redis_connection
 from etl.utils import calc_change, now, s_to_ms, str_format_money
 
 
 def normalize_price_history(raw_history_data):
-    history = []
-    for n, raw_price in enumerate(raw_history_data["prices"]):
-        history.append(
+    price_history = []
+    reduced_raw_history = raw_history_data["prices"][0:-1]
+    for n, raw_price in enumerate(reduced_raw_history):
+        price_history.append(
             {
                 "ts": raw_price[0],
                 "price": round(raw_price[1], 2),
                 "volume": round(raw_history_data["total_volumes"][n][1]),
             }
         )
-    return history
+    return price_history
 
 
 def normalize_tvl_history(raw_tvl_data):
     tvl_history = []
-    for raw_tvl in raw_tvl_data:
+    reduced_raw_tvl_history = raw_tvl_data[-14:]
+    for raw_tvl in reduced_raw_tvl_history:
         tvl_history.append(
             {
                 "ts": s_to_ms(raw_tvl["date"]),
@@ -32,22 +35,34 @@ def normalize_tvl_history(raw_tvl_data):
     return tvl_history
 
 
-def normalize_markets_data(raw_price_data, raw_history_data, raw_tvl_data):
+def merge_price_tvl_history(price_history, tvl_history):
+    merged_history = []
+    for n, price in enumerate(price_history):
+        merged_history.append(
+            merge(price, tvl_history[n])
+        )
+    return merged_history
+
+
+def normalize_markets_data(raw_price_data, raw_price_history, raw_tvl_history):
+    price_history = normalize_price_history(raw_price_history)
+    tvl_history = normalize_tvl_history(raw_tvl_history)
+    merged_history =  merge_price_tvl_history(price_history, tvl_history)
+
     return {
         "price": str_format_money(raw_price_data["solana"]["usd"]),
-        "tvl": raw_tvl_data[-1]["totalLiquidityUSD"],
+        "tvl": raw_tvl_history[-1]["totalLiquidityUSD"],
         "tvlChange": round(
             calc_change(
-                raw_tvl_data[-2]["totalLiquidityUSD"],
-                raw_tvl_data[-1]["totalLiquidityUSD"],
+                raw_tvl_history[-2]["totalLiquidityUSD"],
+                raw_tvl_history[-1]["totalLiquidityUSD"],
             ),
             2,
         ),
         "volume": round(raw_price_data["solana"]["usd_24h_vol"]),
         "change": round(raw_price_data["solana"]["usd_24h_change"], 2),
         "marketCap": round(raw_price_data["solana"]["usd_market_cap"]),
-        "history": normalize_price_history(raw_history_data),
-        "tvlHistory": normalize_tvl_history(raw_tvl_data),
+        "history": merged_history
     }
 
 
@@ -68,7 +83,7 @@ def update_markets():
         "https://api.coingecko.com/api/v3/coins/solana/market_chart",
         params={
             "vs_currency": "usd",
-            "days": 13,
+            "days": 14,
             "interval": "daily",
         },
     ).json()
