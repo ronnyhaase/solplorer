cc7e5065c9dbb96fb0283936a1e07488c094acef
Ronny Haase <ronnyhaase@fastmail.com>
2022-12-15 11:44:24 +0100

    [solplorer] Integrate TableRenderer in NFT collections

--- solplorer/src/pages/nfts.tsx
+++ solplorer/src/pages/nfts.tsx
@@ -13,13 +13,11 @@ import {
   Grid,
   NumberDisplay,
   Panel,
-  Table,
-  TBody,
-  TD,
   TH,
-  THead,
   TR,
 } from '../components'
+import { TableRenderer } from '../components/table-renderer'
+import { sortTableData } from '../components/table-renderer/helper'
 
 export default function NftCollectionsPage({ nftCollectionsData }) {
   return (
@@ -31,93 +29,102 @@ export default function NftCollectionsPage({ nftCollectionsData }) {
         <Container>
           <Grid columns={1}>
             <Panel>
-              <Table>
-                <THead>
-                  <TR>
-                    <TH colSpan={4}>{' '}</TH>
-                    <TH colSpan={3}>Price</TH>
-                    <TH colSpan={3}>Volume</TH>
-                    <TH colSpan={3}>Supply</TH>
-                  </TR>
-                  <TR>
-                    <TH>#</TH>
-                    <TH>Collection</TH>
-                    <TH><Box className="VisuallyHidden">Links</Box></TH>
-                    <TH>Market Cap.</TH>
-                    <TH>Floor</TH>
-                    <TH>Avg</TH>
-                    <TH>High</TH>
-                    <TH>1h</TH>
-                    <TH>24h</TH>
-                    <TH>7d</TH>
-                    <TH>Listed</TH>
-                    <TH>Holders</TH>
-                    <TH>Total</TH>
-                  </TR>
-                </THead>
-                <TBody>
-                  {nftCollectionsData.data.map((collection, n) => (
-                    <TR key={collection.hyperspace_id}>
-                      <TD>{n + 1}</TD>
-                      <TD>
-                        <Box
-                          className="d-flex items-center py-xs overflow-hidden text-ellipsis whitespace-nowrap"
-                          style={{ maxWidth: '28ch' }}
-                        >
-                            <Image
-                              alt={collection.name}
-                              src={`https://ckaumumkea.cloudimg.io/${collection.imageUrl}?w=128&q=80`}
-                              className="rounded mr-sm overflow-hidden"
-                              width={48}
-                              height={48}
-                            />
-                            {' '}
-                            {collection.name}
-                        </Box>
-                      </TD>
-                      <TD>
-                        {collection.urlWebsite
-                          ? <Link className="ml-sm" href={collection.urlWebsite}>
-                            <FaLink title="Website" />
-                          </Link>
-                          : null}
-                        {collection.urlTwitter
-                          ? <Link className="ml-sm" href={collection.urlTwitter}>
-                            <FaTwitter title="Twitter" />
-                          </Link>
-                          : null}
-                        {collection.urlDiscord
-                          ? <Link className="ml-sm" href={collection.urlDiscord}>
-                            <FaDiscord title="Discord" />
-                          </Link>
-                          : null}
-                      </TD>
-                      <TD>
-                        <CurrencyDisplay currency="USD" short val={collection.marketCap} />
-                      </TD>
-                      <TD>
-                        <NumberDisplay prefix="◎ "  val={collection.price.floor} />
-                        {' '}
-                        <ChangeDisplay percent val={collection.price.floorChangePercent_24h} />
-                      </TD>
-                      <TD>
-                        <NumberDisplay prefix="◎ " val={collection.price.avg} />
-                        {' '}
-                        <ChangeDisplay percent val={collection.price.avgChangePercent_24h} />
-                      </TD>
-                      <TD>
-                        <NumberDisplay prefix="◎ " val={collection.price.max} />
-                      </TD>
-                      <TD><CurrencyDisplay currency="USD" short val={collection.volume['1h']} /></TD>
-                      <TD><CurrencyDisplay currency="USD" short val={collection.volume['24h']} /></TD>
-                      <TD><CurrencyDisplay currency="USD" short val={collection.volume['7day']} /></TD>
-                      <TD><NumberDisplay short val={collection.supply.listed} /></TD>
-                      <TD><NumberDisplay short val={collection.supply.holders} /></TD>
-                      <TD><NumberDisplay short val={collection.supply.total} /></TD>
+              {nftCollectionsData ? <TableRenderer
+                renderHeadContent={({ children }) => (
+                  <>
+                    <TR>
+                      <TH colSpan={4}>{' '}</TH>
+                      <TH colSpan={3}>Price</TH>
+                      <TH colSpan={3}>Volume</TH>
+                      <TH colSpan={3}>Supply</TH>
                     </TR>
-                  ))}
-                </TBody>
-              </Table>
+                    {children}
+                  </>
+                )}
+                columns={[
+                  { id: 'name', title: 'Collection', sortable: true, renderContent: (collection) => (
+                    <Box
+                      className="d-flex items-center py-xs overflow-hidden text-ellipsis whitespace-nowrap"
+                      style={{ maxWidth: '28ch' }}
+                    >
+                        <Image
+                          alt={collection.name}
+                          src={`https://ckaumumkea.cloudimg.io/${collection.imageUrl}?w=128&q=80`}
+                          className="rounded mr-sm overflow-hidden"
+                          width={48}
+                          height={48}
+                        />
+                        {' '}
+                        {collection.name}
+                    </Box>
+                  )},
+                  { id: 'links', title: 'Links', sortable: false, renderContent: (collection) => (
+                    <>
+                      {collection.urlWebsite
+                        ? <Link className="ml-sm" href={collection.urlWebsite}>
+                          <FaLink title="Website" />
+                        </Link>
+                        : null}
+                      {collection.urlTwitter
+                        ? <Link className="ml-sm" href={collection.urlTwitter}>
+                          <FaTwitter title="Twitter" />
+                        </Link>
+                        : null}
+                      {collection.urlDiscord
+                        ? <Link className="ml-sm" href={collection.urlDiscord}>
+                          <FaDiscord title="Discord" />
+                        </Link>
+                        : null}
+                    </>
+                  )},
+                  { id: 'marketCap', title: 'Market Cap.', sortable: true, renderContent: (collection) => (
+                    <CurrencyDisplay currency="USD" short val={collection.marketCap} />
+                  )},
+                  { id: 'price.floor', title: 'Floor', sortable: true, renderContent: (collection) => (
+                    <>
+                      <NumberDisplay prefix="◎ "  val={collection.price.floor} />
+                      {' '}
+                      <ChangeDisplay percent val={collection.price.floorChangePercent_24h} />
+                    </>
+                  )},
+                  { id: 'price.avg', title: 'Avg', sortable: true, renderContent: (collection) => (
+                    <>
+                      <NumberDisplay prefix="◎ " val={collection.price.avg} />
+                      {' '}
+                      <ChangeDisplay percent val={collection.price.avgChangePercent_24h} />
+                    </>
+                  )},
+                  { id: 'price.max', title: 'High', sortable: true, renderContent: (collection) => (
+                    <NumberDisplay prefix="◎ " val={collection.price.max} />
+                  )},
+                  { id: 'volume.1h', title: '1h', sortable: true, renderContent: (collection) => (
+                    <CurrencyDisplay currency="USD" short val={collection.volume['1h']} />
+                  )},
+                  { id: 'volume.24h', title: '24h', sortable: true, renderContent: (collection) => (
+                    <CurrencyDisplay currency="USD" short val={collection.volume['24h']} />
+                  )},
+                  { id: 'volume.7day', title: '7day', sortable: true, renderContent: (collection) => (
+                    <CurrencyDisplay currency="USD" short val={collection.volume['7day']} />
+                  )},
+                  { id: 'supply.listed', title: 'Listed', sortable: true, renderContent: (collection) => (
+                    <NumberDisplay short val={collection.supply.listed} />
+                  )},
+                  { id: 'supply.holders', title: 'Holders', sortable: true, renderContent: (collection) => (
+                    <NumberDisplay short val={collection.supply.holders} />
+                  )},
+                  { id: 'supply.total', title: 'Total', sortable: true, renderContent: (collection) => (
+                    <NumberDisplay short val={collection.supply.total} />
+                  )},
+                ]}
+                data={nftCollectionsData.data}
+                rowKeyColId="hyperspace_id"
+                onSortChange={(col, dir, updateData) => {
+                  const sortedData = nftCollectionsData.data.sort(sortTableData(col, dir))
+                  updateData(sortedData)
+                }}
+                sortingColId="marketCap"
+                sortingDirection="DESC"
+              /> : null}
               <div>
                   <div className="d-flex items-center justify-center my-md">
                     <span>Data provided by</span>
